30a4f1fde861a2aab2ce26616665f2b52fd1d287
Alex Zirbel <alexzirbel@gmail.com>
2017-01-29 11:29:50 -0800

    Add CORS

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
 
+gem 'active_model_serializers'
+gem 'rack-cors'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
 # Use postgresql as the database for Active Record
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module Npoint
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
