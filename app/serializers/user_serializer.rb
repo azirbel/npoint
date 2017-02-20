584168ba4f8ac13fa28f4b21dba962ad715e17c9
Alex Zirbel <alexzirbel@gmail.com>
2017-02-20 14:16:42 -0800

    Remove id and gravatar_url from UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,11 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id,
-    :name,
-    :email,
-    :gravatar_url
-
-  def gravatar_url
-    gravatar_id = Digest::MD5::hexdigest(object.email.downcase)
-    "https://secure.gravatar.com/avatar/#{gravatar_id}"
-  end
+  attributes :name, :email
 end
