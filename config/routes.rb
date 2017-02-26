d50823787762bc4e629412daec868a393f80157b
Alex Zirbel <alexzirbel@gmail.com>
2017-02-25 17:58:21 -0800

    Letsencrypt endpoint for API

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,5 @@ Rails.application.routes.draw do
   # Followed https://collectiveidea.com/blog/archives/2016/01/12/lets-encrypt-with-a-rails-app-on-heroku#comment-56f2af9c524ce84ba3000005
   # to set up SSL
   get '/.well-known/acme-challenge/WMR9ZsaN_jU71mT7d4Z59RBrRZa4Nw80Ms61cfmhXWY' => 'pages#letsencrypt'
+  get '/.well-known/acme-challenge/jEd9Iw3Oi37AY-h2GsmmaFvjE8z3MnUSP1CaR_vMOtM' => 'pages#letsencrypt_api'
 end
