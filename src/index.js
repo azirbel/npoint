4a1a236c6ec95d19a82e251216a98bc372ff8da2
Alex Zirbel <alexzirbel@gmail.com>
2018-03-16 13:39:28 -0700

    Format first file with prettier

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
+// @format
+
 /* global axios */
 
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-import routes from './routes';
-import configureStore from './store/configureStore';
-import ReactModal from 'react-modal';
-import './styles/avatar.css';
-import './styles/badge.css';
-import './styles/banner.css';
-import './styles/buttons.css';
-import './styles/flex.css';
-import './styles/form.css';
-import './styles/grid.css';
-import './styles/index.css';
-import './styles/links.css';
-import './styles/list.css';
-import './styles/modal.css';
-import './styles/sections.css';
-import './styles/typography.css';
-import './styles/variables.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { Router, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import routes from './routes'
+import configureStore from './store/configureStore'
+import ReactModal from 'react-modal'
+import './styles/avatar.css'
+import './styles/badge.css'
+import './styles/banner.css'
+import './styles/buttons.css'
+import './styles/flex.css'
+import './styles/form.css'
+import './styles/grid.css'
+import './styles/index.css'
+import './styles/links.css'
+import './styles/list.css'
+import './styles/modal.css'
+import './styles/sections.css'
+import './styles/typography.css'
+import './styles/variables.css'
 
-axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.headers.common['X-CSRF-Token'] =
-  (document.head.querySelector('meta[name=csrf-token]') || {}).content
+axios.defaults.headers.common['Content-Type'] = 'application/json'
+axios.defaults.headers.common['X-CSRF-Token'] = (
+  document.head.querySelector('meta[name=csrf-token]') || {}
+).content
 
-axios.interceptors.response.use((response) => {
+axios.interceptors.response.use(response => {
   let newToken = response.headers['x-csrf-token']
   if (newToken) {
     axios.defaults.headers.common['X-CSRF-Token'] = newToken
@@ -36,17 +39,17 @@ axios.interceptors.response.use((response) => {
       csrfMeta.setAttribute('content', newToken)
     }
   }
-  return response;
-});
+  return response
+})
 
-const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
+const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
 
-ReactModal.setAppElement('#root');
+ReactModal.setAppElement('#root')
 
 render(
   <Provider store={store}>
     <Router history={history} routes={routes} />
   </Provider>,
   document.getElementById('root')
-);
+)
