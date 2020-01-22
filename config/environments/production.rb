d558285d098218c80833d64ade4d4e52f1e4568e
Alex Zirbel <alexzirbel@gmail.com>
2020-01-21 17:47:44 -0800

    Revert "Turn on additional debugging for render"
    This reverts commit 3d13251ff4323694ef4f93f31b6a48cc10eaffdb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,9 +10,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # TEMP change to debug render production
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
