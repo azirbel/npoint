5442d828f0b3002ada499ecf496ab88ed21b30e7
Alex Zirbel <alexzirbel@gmail.com>
2020-04-12 14:18:38 -0700

    Fix server hanging on start locally

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Do not eager load code on boot.
-  config.eager_load = false
+  # Eager load code. Solved problem where the server was hanging after startup
+  config.eager_load = true
 
   # Show full error reports.
   config.consider_all_requests_local = true
