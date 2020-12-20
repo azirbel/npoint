548087e91c9b49440e49ec270e535d5cbc6359d6
Alex Zirbel <alexzirbel@gmail.com>
2020-12-19 17:56:34 -0800

    Add rack-attack to rate limit. Initial low limit for testing.

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,29 @@
+class Rack::Attack
+  ### Throttle Spammy Clients ###
+
+  # If any single client IP is making tons of requests, then they're
+  # probably malicious or a poorly-configured scraper. Either way, they
+  # don't deserve to hog all of the app server's CPU. Cut them off!
+  #
+  # Note: On a typical document page, refreshing creates 2 requests
+  # (one for the document, one for the user). Doubled the default setting
+  # of 60rpm so a refresh of once per second is allowed
+
+  # Throttle all requests by IP (120rpm)
+  # FOR TESTING: limit to 20
+  throttle('req/ip', limit: 20, period: 5.minutes) do |req|
+    req.ip unless req.path.start_with?('/assets')
+  end
+
+  # Throttle requests per document (10 per second = 600rpm)
+  # FOR TESTING: limit to 3
+  throttle('limit document calls by token', limit: 3, period: 1.minute) do |req|
+    if req.url.match(/^https?:\/\/api./)
+      # API route
+      req.path.match(/\/([A-Za-z0-9]{20})/)&.[](1) # tokens are 20 chars
+    elsif req.path.match(/\/documents\/[A-Za-z0-9]{20}/)
+      # /documents app route
+      req.path.match(/\/documents\/([A-Za-z0-9]{20})/)&.[](1) # tokens are 20 chars
+    end
+  end
+end
